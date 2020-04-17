2ed4ba6df2b2fe7fade5b84581a080ba8fb1ed1d
s17552 <s17552@pjwstk.edu.pl>
2020-04-17 19:58:30 +0200

    copy from cw2

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        /*
+        [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult TestPut(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Sukcess");
+            }
+            return NotFound("Error");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult TestDelete(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Sukcess");
+            }
+            return NotFound("Error");
+        }
+    */
+    }
+}
